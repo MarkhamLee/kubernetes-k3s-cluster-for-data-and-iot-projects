c83d707a54cc05036179dbedbab69f4e20f5087a
Markham <markhamdev@markhamlee.com>
2024-02-10 03:40:26 -0800

    Updated Rockchip3588 Device Monitoring container, try/except, code clean-up

--- hardware_monitoring/rockchip3588/container/main.py
+++ hardware_monitoring/rockchip3588/container/main.py
@@ -8,18 +8,23 @@
 # https://github.com/Joshua-Riek/ubuntu-rockchip
 
 
-import json
 import time
 import gc
 import os
 import logging
+from sys import stdout
 from rockchip_3588 import RockChipData
 from influx_client import InfluxClient
 
-# create logger for logging errors, exceptions and the like
-logging.basicConfig(filename='hardwareDataRockChip.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(name)s %(threadName)s\
-                        : %(message)s')
+# set up/configure logging with stdout so it can be picked up by K8s
+logger = logging.getLogger('rockchip_telemetry_logger')
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(message)s')  # noqa: E501
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 # instantiate InfluxDB class
 influxdb_write = InfluxClient()
@@ -40,45 +45,51 @@ def monitor(client: object, bucket, table, interval):
 
     while True:
 
-        # get CPU utilization
-        cpuUtil = rockchip_data.getCPUData()
+        try:
+            # get CPU utilization
+            cpuUtil = rockchip_data.getCPUData()
 
-        # get current RAM use
-        ramUse = rockchip_data.getRamData()
+            # get current RAM use
+            ramUse = rockchip_data.getRamData()
 
-        # get per CPU frequencies (bigCore0, bigCore1, littleCore)
-        littleCoreFreq, bigCore0Freq, bigCore1Freq = rockchip_data.\
-            getRockChip3588Freqs()
+            # get per CPU frequencies (bigCore0, bigCore1, littleCore)
+            littleCoreFreq, bigCore0Freq, bigCore1Freq = rockchip_data.\
+                getRockChip3588Freqs()
 
-        # get system temperatures
-        socTemp, bigCore0Temp, bigCore1Temp, littleCoreTemp, centerTemp, \
-            gpuTemp, npuTemp, nvmeTemp = rockchip_data.sysTemps()
+            # get system temperatures
+            socTemp, bigCore0Temp, bigCore1Temp, littleCoreTemp, centerTemp, \
+                gpuTemp, npuTemp, nvmeTemp = rockchip_data.sysTemps()
 
-        payload = {
-            "SOC": socTemp,
-            "bigCore0": bigCore0Temp,
-            "bigCore1": bigCore1Temp,
-            "littleCore": littleCoreTemp,
-            "Center": centerTemp,
-            "GPU": gpuTemp,
-            "NPU": npuTemp,
-            "NVME": nvmeTemp,
-            "littleCoreFreq": littleCoreFreq,
-            "bigCore0Freq": bigCore0Freq,
-            "bigCore1Freq": bigCore1Freq,
-            "cpuUse": cpuUtil,
-            "ramUse": ramUse
-        }
+            payload = {
+                "SOC": socTemp,
+                "bigCore0": bigCore0Temp,
+                "bigCore1": bigCore1Temp,
+                "littleCore": littleCoreTemp,
+                "Center": centerTemp,
+                "GPU": gpuTemp,
+                "NPU": npuTemp,
+                "NVME": nvmeTemp,
+                "littleCoreFreq": littleCoreFreq,
+                "bigCore0Freq": bigCore0Freq,
+                "bigCore1Freq": bigCore1Freq,
+                "cpuUse": cpuUtil,
+                "ramUse": ramUse
+            }
 
-        payload = json.dumps(payload)
+            # write data to InfluxDB
+            influxdb_write.write_influx_data(client, base_payload,
+                                             payload, bucket)
 
-        # write data to InfluxDB
-        influxdb_write.write_influx_data(client, base_payload, payload, bucket)
+            del payload, socTemp, bigCore0Temp, bigCore1Temp, \
+                littleCoreTemp, centerTemp, gpuTemp, npuTemp, nvmeTemp,
+            littleCoreFreq, bigCore0Freq, bigCore1Freq, cpuUtil, ramUse
+            gc.collect()
 
-        del payload, socTemp, bigCore0Temp, bigCore1Temp, \
-            littleCoreTemp, centerTemp, gpuTemp, npuTemp, nvmeTemp,
-        littleCoreFreq, bigCore0Freq, bigCore1Freq, cpuUtil, ramUse
-        gc.collect()
+        except Exception as e:
+            logger.debug(f'failed to read data from device with error: {e}')
+            # we'll just log the errors for now, if the device is truly
+            # malfunctioning alert manager will pick it up as a
+            # node error/problem.
 
         time.sleep(interval)
 
@@ -90,8 +101,7 @@ def main():
     URL = os.environ['URL']
     BUCKET = os.environ['BUCKET']
     TABLE = os.environ['TABLE']
-
-    INTERVAL = os.environ('INTERVAL')
+    INTERVAL = int(os.environ['INTERVAL'])
 
     # get client
     client = influxdb_write.influx_client(TOKEN, ORG, URL)
