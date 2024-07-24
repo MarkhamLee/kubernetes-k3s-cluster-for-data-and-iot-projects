4eaffc66d9e405599862ff38c2d10ad2e5c644e6
Markham Lee <markhamdev@markhamlee.com>
2024-07-24 11:01:37 -0700

    Archive folder w/ old approach, re-writing all of these to push data via MQTT to enable future capabilities leveraging two way coms

---
+++ hardware_monitoring/old_versions/amd_5560u/main.py
@@ -0,0 +1,85 @@
+# Markham Lee (C) 2023 - 2024
+# kubernetes-k3s-data-and-IoT-platform
+# https://github.com/MarkhamLee/k3s-data-platform-IoT
+# Script to monitor temperatures of the AMD 5560u on the
+# Beelink SER5s that are being used for the cluster nodes.
+# Only monitoring the temps (for now), because I'm using the
+# kube-prometheus stack to monitor CPU load, disk space, RAM, etc.
+
+import gc
+import os
+import sys
+from time import sleep
+from amd_5560u import AMD5560Data
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+from hw_monitoring_libraries.logging_util import logger  # noqa: E402
+from hw_monitoring_libraries.influx_client import InfluxClient  # noqa: E402
+
+# instantiate InfluxDB class
+influxdb_write = InfluxClient()
+
+
+def get_base_payload(table):
+
+    # base payload
+    base_payload = {
+        "measurement": table,
+        "tags": {
+                "k3s_prod": "hardware_telemetry",
+        }
+    }
+
+    return base_payload
+
+
+def monitor(client: object, get_data: object, BUCKET: str, ORG: str,
+            TABLE: str, DEVICE_ID: str, INTERVAL: str):
+
+    logger.info(f'Starting HW monitoring for {DEVICE_ID}')
+
+    while True:
+
+        # get CPU, GPU and NVME temperatures
+        nvme_temp, cpu_temp, amd_gpu_temp = get_data.amd_linux_data()
+
+        # get base_payload
+        base_payload = get_base_payload(TABLE)
+
+        payload = {
+            "nvme_temp": nvme_temp,
+            "cpu_temp": cpu_temp,
+            "amd_gpu_temp": amd_gpu_temp
+        }
+
+        # write data to InfluxDB
+        influxdb_write.write_influx_data(client, base_payload, payload, BUCKET)
+
+        del nvme_temp, cpu_temp, amd_gpu_temp, payload, base_payload
+        gc.collect()
+        sleep(INTERVAL)
+
+
+def main():
+
+    DEVICE_ID = os.environ['DEVICE_ID']
+    INTERVAL = int(os.environ['INTERVAL'])
+    TOKEN = os.environ['TOKEN']
+    ORG = os.environ['ORG']
+    URL = os.environ['URL']
+    BUCKET = os.environ['BUCKET']
+    TABLE = os.environ['TABLE']
+
+    # instantiate utilities class
+    device_data = AMD5560Data()
+
+    # get client
+    client = influxdb_write.influx_client(TOKEN, ORG, URL)
+
+    monitor(client, device_data, BUCKET, ORG, TABLE, DEVICE_ID, INTERVAL)
+
+
+if __name__ == '__main__':
+    main()
---
+++ hardware_monitoring/old_versions/intel_x86/intel_x86.py
@@ -0,0 +1,74 @@
+# Markham Lee (C) 2023
+# Hardware Monitor for Linux & Windows:
+# # https://github.com/MarkhamLee/k3s-data-platform-IoT
+# script to retrieve CPU related data on an Intel x86 machine
+# and then write to InfluxDB
+import json
+import psutil
+
+
+class Intelx86():
+
+    def __init__(self):
+
+        # get the # of cores, as we can use that to iterate through and
+        # get things like current speed for all CPU cores
+        self.core_count = psutil.cpu_count(logical=False)
+
+    def build_payload(self, inputFunction, index=0):
+
+        temp_dict = {}
+
+        while self.core_count > index:
+
+            data = inputFunction[index].current
+            data = round(data, 1)
+            key = (f'core {index}')
+            temp_dict[key] = data
+            index += 1
+
+        payload = json.dumps(temp_dict)
+
+        return payload
+
+    # get average clock speed for all cores
+    def get_freq(self, all_cpu=False):
+
+        all_freq = psutil.cpu_freq(percpu=all_cpu)[0]
+        all_freq = round(all_freq, 1)
+
+        return all_freq, self.core_count
+
+    # get frequency per core
+    @staticmethod
+    def freq_per_core(self, all_cpu=True):
+
+        per_core_freq = self.buildPayload(psutil.cpu_freq(percpu=all_cpu))
+
+        return per_core_freq
+
+    # CPU load
+    @staticmethod
+    def get_cpu_data():
+
+        cpu_util = (psutil.cpu_percent(interval=1))
+        cpu_util = round(cpu_util, 1)
+
+        return cpu_util
+
+    # get current RAM used
+    @staticmethod
+    def get_ram_data():
+
+        ram_use = (psutil.virtual_memory()[3]) / 1073741824
+        ram_use = round(ram_use, 2)
+
+        return ram_use
+
+    @staticmethod
+    def get_temps():
+
+        core_temp = psutil.sensors_temperatures()['coretemp'][0].current
+        nvme_temp = psutil.sensors_temperatures()['nvme'][0].current
+
+        return core_temp, nvme_temp
