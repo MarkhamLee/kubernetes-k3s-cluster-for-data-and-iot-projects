5702fa62ed517af253322b7ee4a14c6da70f4c54
Markham Lee <markhamdev@markhamlee.com>
2025-10-05 21:18:22 -0700

    Checking in updated Raspberry Pi 4B monitoring code

--- hardware_monitoring/raspberrypi_4b/main.py
+++ hardware_monitoring/raspberrypi_4b/main.py
@@ -2,30 +2,63 @@
 # kubernetes-k3s-data-and-IoT-platform
 # https://github.com/MarkhamLee/kubernetes-k3s-data-and-IoT-Platform
 # Script for monitoring a Raspberry Pi 4B, pulling CPU utilization,
-# temps and clock speed and RAM utilization data. The Data is transmitted
-# via MQTT because it a) uses less power than API requests b) the
-# communication is two way, which will be used to light up future device
-# mgt capabilities in the future.
-import gc
-import json
+# temps and clock speed and RAM utilization data.
 import os
 import sys
 import time
+from prometheus_client import Gauge, start_http_server
 from rpi4b_data import Rpi4bData
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 
-from hw_monitoring_libraries.logging_util import logger  # noqa: E402
-from hw_monitoring_libraries.hw_monitoring\
-    import MonitoringUtilities  # noqa: E402
+from hw_monitoring_libraries.\
+    logging_util import logger  # noqa: E402
+from hw_monitoring_libraries.\
+    influx_client import InfluxClient  # noqa: E402
+
+# load environmental variables
+BUCKET = os.environ['INFLUX_BUCKET']
+DEVICE_ID = os.environ['DEVICE_ID']
+HEARTBEAT_FLAG = int(os.environ['HEARTBEAT_FLAG'])
+INTERVAL = int(os.environ['INTERVAL'])
+METRIC_IP = os.environ['METRIC_IP']
+METRIC_PORT = int(os.environ['METRIC_PORT'])
+ORG = os.environ['INFLUX_ORG']
+TABLE = os.environ['INFLUX_MEASUREMENT']
+TOKEN = os.environ['INFLUX_TOKEN']
+URL = os.environ['INFLUX_URL']
+TAG_KEY = os.environ['TAG_KEY']
+TAG_VALUE = os.environ['TAG_VALUE']
+
+# instantiate InfluxDB class
+influxdb_write = InfluxClient()
+
+# instantiate RPI4B data retrieval class
+get_data = Rpi4bData()
+logger.info('Data class instantiated')
+
+
+logger.info('Creating base payload for writing to InfluxDB')
+base_payload = {
+    "measurement": TABLE,
+    "tags": {
+            TAG_KEY: TAG_VALUE,
+    }
+}
+
+
+if HEARTBEAT_FLAG == 1:
+    logger.info('Settiing up Prometheus heartbeat metric export')
+    METRIC_NAME = (f'{DEVICE_ID}_HEARTBEAT')
+    heartbeat = Gauge(METRIC_NAME, DEVICE_ID)
+
+    logger.info('Starting metric server')
+    start_http_server(METRIC_PORT, addr=METRIC_IP)
 
 
 # method that runs monitoring loop
-def monitor(client: object, get_data: object, topic: str):
-
-    DEVICE_ID = os.environ['DEVICE_ID']
-    INTERVAL = int(os.environ['INTERVAL'])
+def monitor(client: object):
 
     logger.info(f'Starting HW monitoring for {DEVICE_ID}')
 
@@ -50,52 +83,24 @@ def monitor(client: object, get_data: object, topic: str):
             "ramUse": ram_use
         }
 
-        payload = json.dumps(payload)
-        result = client.publish(topic, payload)
-        status = result[0]
+        # write data to InfluxDB
+        influxdb_write.write_influx_data(client,
+                                         base_payload,
+                                         payload,
+                                         BUCKET)
 
-        if status != 0:
+        if HEARTBEAT_FLAG == 1:
+            heartbeat.set(1)
 
-            logger.debug(f'MQTT publishing failure for hardware monitoring on: {DEVICE_ID}, return code: {status}')  # noqa: E501
-
-        del payload, cpu_util, ram_use, cpu_freq, cpu_temp, status, result
-        gc.collect()
         time.sleep(INTERVAL)
 
 
 def main():
 
-    # instantiate RPI4B data retrieval class
-    get_data = Rpi4bData()
-    logger.info('Data class instantiated')
+    # get client
+    client = influxdb_write.influx_client(TOKEN, ORG, URL)
 
-    # instantiate hardware monitoring class
-    monitor_utilities = MonitoringUtilities()
-    logger.info('Monitoring utilities class instantiated')
-
-    # operating parameters
-    TOPIC = os.environ['TOPIC']
-
-    # load environmental variables
-    MQTT_BROKER = os.environ["MQTT_BROKER"]
-    MQTT_USER = os.environ['MQTT_USER']
-    MQTT_SECRET = os.environ['MQTT_SECRET']
-    MQTT_PORT = int(os.environ['MQTT_PORT'])
-
-    # get unique client ID
-    clientID = monitor_utilities.getClientID()
-
-    # get mqtt client
-    client, code = monitor_utilities.mqttClient(clientID,
-                                                MQTT_USER, MQTT_SECRET,
-                                                MQTT_BROKER, MQTT_PORT)
-
-    # start monitoring
-    try:
-        monitor(client, get_data, TOPIC)
-
-    finally:
-        client.loop_stop()
+    monitor(client)
 
 
 if __name__ == '__main__':
